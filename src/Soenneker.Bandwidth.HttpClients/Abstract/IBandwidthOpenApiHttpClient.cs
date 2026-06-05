c6a99aa8945a29b9a571f7e36150ae999f69486d
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:57 -0500

    xml commenting

--- src/Soenneker.Bandwidth.HttpClients/Abstract/IBandwidthOpenApiHttpClient.cs
+++ src/Soenneker.Bandwidth.HttpClients/Abstract/IBandwidthOpenApiHttpClient.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Bandwidth.HttpClients.Abstract;
 /// </summary>
 public interface IBandwidthOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
